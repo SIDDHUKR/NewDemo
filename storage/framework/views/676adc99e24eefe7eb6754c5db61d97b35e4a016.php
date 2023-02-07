b533612b4c3eef6fb3c41fd965b23c299c2dae76
SIDDHUKR <118068703+SIDDHUKR@users.noreply.github.com>
2023-02-07 22:52:46 +0530

    Add files via upload

---
+++ storage/framework/views/676adc99e24eefe7eb6754c5db61d97b35e4a016.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <p>&copy;copyright:2023</p>
+</div>
+
+<style>
+    .footer{
+        background-color: #bbbbbb; color: black;
+        width: 100w; height: 4vh; text-align: center;
+    }
+
+
+
+     /* ---------For Mobile--------- */
+     @media (max-width:650px){
+
+        .footer{
+        width: 100vw;
+    }
+
+}
+
+</style>
+<?php /**PATH C:\xampp\htdocs\NewDemo\resources\views/footer.blade.php ENDPATH**/ ?>
